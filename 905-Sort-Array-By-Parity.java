ce7d1642710e013cc981e678fe3342540f59670f
Mai Samir <88589466+maiSamirMohammad@users.noreply.github.com>
2024-07-15 20:43:28 +0300

    [905] [Time Beats: 100.00%] [Memory Beats: 82.10%] - LeetPush

---
+++ 905-Sort-Array-By-Parity.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int l = 0;
+
+        for (int r = 0; r < nums.length; r++) {
+            if (nums[r] % 2 == 0) {
+                int temp = nums[l];
+                nums[l] = nums[r];
+                nums[r] = temp;
+                l++;
+            }
+        }
+
+        return nums;
+    }
+}
