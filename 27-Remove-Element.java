205997005c1430a47688f78321eb7ee2591194ad
Mai Samir <88589466+maiSamirMohammad@users.noreply.github.com>
2024-07-16 22:53:25 +0300

    [27] [Time Beats: 100.00%] [Memory Beats: 21.81%] - LeetPush

---
+++ 27-Remove-Element.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int l = 0;
+        for(int r = 0; r < nums.length ; r++){
+            if (nums[r]!= val){
+                nums[l]= nums[r];
+                l+=1;
+            }
+        }
+        return l;
+    }
+}
