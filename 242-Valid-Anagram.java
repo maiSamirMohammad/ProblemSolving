b75d7b9d428a79a6fc41ca9722b8f0b52ddb655b
Mai Samir <88589466+maiSamirMohammad@users.noreply.github.com>
2024-08-06 04:26:32 +0300

    [242] [Time Beats: 5.45%] [Memory Beats: 50.68%] - LeetPush

--- 242-Valid-Anagram.java
+++ 242-Valid-Anagram.java
@@ -3,18 +3,22 @@ class Solution {
          if (s.length() != t.length()) {
             return false;
         }
+        
         Map<Character, Integer> sMap = new HashMap();
         Map<Character, Integer> tMap = new HashMap();
         for(int i = 0; i<s.length(); i++){
+           //    put(    key    ,  value )
             sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), 0) + 1);
             tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), 0) + 1);
         }
          for (char c : sMap.keySet()) {
-            if (!tMap.getOrDefault(c, 0).equals(sMap.get(c))) {
+            if (!sMap.get(c).equals(tMap.getOrDefault(c, 0))) {
                 return false;
             }
         }
         
         return true;
+
+        
     }
 }
