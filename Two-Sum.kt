72ab5e20ca7a56e83a3c194a87bc1fe6ebadea7f
Mai Samir <88589466+maiSamirMohammad@users.noreply.github.com>
2025-04-27 20:37:56 +0300

    [1] [Time Beats: 24.80%] [Memory Beats: 38.87%] - LeetPush

--- Two-Sum.kt
+++ Two-Sum.kt
@@ -1,10 +1,7 @@
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
         val myList = mutableListOf<Int>()
-        
-        // Classic for loop for index1
         for (index1 in nums.indices) {
-            // Classic for loop for index2, starting from index1 + 1
             for (index2 in index1 + 1 until nums.size) {
                 if (nums[index1] + nums[index2] == target) {
                     myList.add(index1)
@@ -13,7 +10,6 @@ class Solution {
                 }
             }
         }
-        
         return myList.toIntArray()
     }
 }
