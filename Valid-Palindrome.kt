a1c5784a145331093923925bc4788f9182ff15a8
Mai Samir <88589466+maiSamirMohammad@users.noreply.github.com>
2025-05-15 06:32:31 +0300

    [125] [Time Beats: 37.07%] [Memory Beats: 48.39%] - LeetPush

--- Valid-Palindrome.kt
+++ Valid-Palindrome.kt
@@ -1,21 +1,6 @@
 class Solution {
     fun isPalindrome(s: String): Boolean {
-        var left=0
-        var right=s.length-1
-        while (left < right){
-            while(left< right && !isAlphanumeric(s[left])){
-                left++
-            }
-            while(left< right && !isAlphanumeric(s[right])){
-                right--
-            }
-            if (s[left].lowercaseChar() != s[right].lowercaseChar()){
-                return false
-            }
-            left++
-            right--
-        }
-        return true
+    val modifiedString= s.lowercase().filter{ it.isLetterOrDigit()}
+    return modifiedString==modifiedString.reversed()
     }
-    private fun isAlphanumeric(c:Char)=(c in 'a'..'z') || (c in 'A'..'Z') || (c in '0'..'9')
 }
